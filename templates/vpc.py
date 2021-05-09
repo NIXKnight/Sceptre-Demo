737c881e9193520ad3e24618dbcda7d1047c18e6
Saad Ali <saad@nixknight.net>
2021-05-10 00:45:51 +0500

    Create public subnets

--- templates/vpc.py
+++ templates/vpc.py
@@ -1,5 +1,5 @@
 from troposphere import Template, Ref, Output, Export, Sub
-from troposphere.ec2 import VPC, InternetGateway, VPCGatewayAttachment
+from troposphere.ec2 import VPC, InternetGateway, VPCGatewayAttachment, Subnet
 
 class vpc(object):
 
@@ -12,10 +12,26 @@ class vpc(object):
     # Internet Gateway vars
     self.igw_name = "InternetGateway"
     self.igw_attachment = str(self.igw_name + "Attachment")
+    # Public Subnet vars
+    self.public_subnets = sceptre_user_data['resources']['vpc']['subnets']['public']
 
     # Create stack resources
     self.create_vpc()
     self.create_igw()
+    self.create_subnets()
+
+  ## Generic VPC functions
+  # Create VPC Subnet resource
+  def create_subnet(self, subnet_name, cidr_block, vpc_name, availability_zone):
+    t = self.template
+    t.add_resource(
+      Subnet(
+        subnet_name,
+        CidrBlock=cidr_block,
+        VpcId=Ref(vpc_name),
+        AvailabilityZone=availability_zone
+      )
+    )
 
   # Define CloudFormation VPC resource
   def create_vpc(self):
@@ -54,6 +70,15 @@ class vpc(object):
       )
     )
 
+  # Define CloudFormation Subnet resources
+  def create_subnets(self):
+    public_subnets = self.public_subnets
+    for list_item in range(len(public_subnets)):
+      name = public_subnets[list_item]["name"]
+      cidr = public_subnets[list_item]["cidr"]
+      vpc_name = self.vpc_name
+      az = public_subnets[list_item]["az"]
+      self.create_subnet(name, cidr, vpc_name, az)
 
 # Return CloudFormation stack as a string
 def sceptre_handler(sceptre_user_data):
